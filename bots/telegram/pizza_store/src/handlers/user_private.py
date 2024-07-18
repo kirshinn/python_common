21e643be94220f325f77ea367cc5c71d8fb18961
Kirshin Aleksandr <aleksandr.s.kirshin@gmail.com>
2024-07-18 09:50:05 +0700

    commit 20240718_1000

--- bots/telegram/pizza_store/src/handlers/user_private.py
+++ bots/telegram/pizza_store/src/handlers/user_private.py
@@ -1,5 +1,6 @@
 from aiogram import F, types, Router
 from aiogram.filters import CommandStart, Command, or_f
+from aiogram.enums import ParseMode
 
 from filters.chat_types import ChatTypeFilter
 from keyboards import reply
@@ -10,12 +11,16 @@ user_private_router.message.filter(ChatTypeFilter(['private']))
 
 @user_private_router.message(CommandStart())
 async def cmd_start(message: types.Message) -> None:
-    await message.answer('Привет, я виртуальный помощник!', reply_markup=reply.keyboard_start)
+    await message.answer('Привет, я виртуальный помощник!',
+                         reply_markup=reply.keyboard_start_builder_attach.as_markup(
+                             resize_keyboard=True,
+                             input_field_placeholder='Что вас интересует?',
+                         ))
 
 
 @user_private_router.message(or_f(Command('menu'), F.text.lower() == 'меню'))
 async def cmd_menu(message: types.Message) -> None:
-    await message.answer('меню')
+    await message.answer('меню', reply_markup=reply.keyboard_delete)
 
 
 @user_private_router.message(Command('about'))
@@ -32,4 +37,16 @@ async def cmd_menu(message: types.Message) -> None:
 @user_private_router.message(Command('shipping'))
 @user_private_router.message(F.text, F.text.lower().contains('доставк'))
 async def cmd_menu(message: types.Message) -> None:
-    await message.answer('варианты доставки')
+    await message.answer('<b>варианты доставки</b>', parse_mode=ParseMode.HTML)
+
+
+@user_private_router.message(F.contact)
+async def get_contact(message: types.Message) -> None:
+    await message.answer(f'номер получен')
+    await message.answer(str(message.contact))
+
+
+@user_private_router.message(F.location)
+async def get_location(message: types.Message) -> None:
+    await message.answer(f'локация получена')
+    await message.answer(str(message.location))
--- bots/telegram/pizza_store/src/keyboards/reply.py
+++ bots/telegram/pizza_store/src/keyboards/reply.py
@@ -1,14 +1,54 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton, KeyboardButtonPollType
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
+# 1 способ создания клавиатуры
 keyboard_start = ReplyKeyboardMarkup(
     keyboard=[
         [
             KeyboardButton(text='Меню'),
             KeyboardButton(text='О магазине'),
+        ],
+        [
             KeyboardButton(text='Варианты оплаты'),
             KeyboardButton(text='Варианты доставки'),
         ]
     ],
     resize_keyboard=True,
     input_field_placeholder='Что вас интересует?',
+)
+
+# способ удаления клавиатуры
+keyboard_delete = ReplyKeyboardRemove()
+
+# 2 способ создания клавиатуры
+keyboard_start_builder = ReplyKeyboardBuilder()
+
+keyboard_start_builder.add(
+    KeyboardButton(text='Меню'),
+    KeyboardButton(text='О магазине'),
+    KeyboardButton(text='Варианты оплаты'),
+    KeyboardButton(text='Варианты доставки'),
+)
+
+keyboard_start_builder.adjust(2, 2)
+
+# расширение существующей клавиатуры
+keyboard_start_builder_attach = ReplyKeyboardBuilder()
+
+keyboard_start_builder_attach.attach(keyboard_start_builder)
+
+keyboard_start_builder_attach.row(
+    KeyboardButton(text='Оставить отзыв'),
+)
+
+# клавиатура с запросом номера и местоположения
+keyboard_query = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Создать Опрос', request_poll=KeyboardButtonPollType()),
+            KeyboardButton(text='Отправить номер', request_contact=True),
+            KeyboardButton(text='Отправить местоположение', request_location=True),
+        ],
+    ],
+    resize_keyboard=True,
 )
